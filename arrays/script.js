d292ddf7fba9f80df7427ebec58843403fa12412
Harshit Kumar <kumarharshit6206@gmail.com>
2023-08-26 21:31:37 +0530

    js

---
+++ arrays/script.js
@@ -0,0 +1,25 @@
+const nums =[3,4,6,3,1,7,8,5];
+
+//console.log(nums.indexOf(3))
+//console.log(nums.includes(2))
+// nums.push('harsh')
+// console.log(nums)
+
+// nums.pop();
+// console.log(nums)
+
+
+
+
+
+// const words = ["Apple", "Banana", "Orange", 23, true, {
+//     name: 'anuj'
+// }];
+
+// console.log(words.length);
+
+// const newwords = words;
+
+// newwords[2] = 33;
+
+// console.log(newwords);
---
+++ functions/scriptt.js
@@ -0,0 +1,23 @@
+const a = 5;
+const b = 6;
+
+sum(a,b)
+// sum = a+b;
+// console.log(sum);
+
+function sum(a , b){
+    const sum = a+b;
+    console.log(sum)
+}
+
+
+// const c = 8;
+// const d = 5;
+// sum = c+d;
+// console.log(c)
+
+
+// function sum(a, b) {
+//     const sum = a+b;
+//     console.log(sum);
+// }
